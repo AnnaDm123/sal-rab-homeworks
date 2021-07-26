6bb39992d8556b8d9dc95fcdaac6b879357d8e8c
Anna DM <floobi@mail.ru>
2021-07-26 13:58:52 +0300

    задание 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -12,31 +12,20 @@ function addInCartMessage(title, price) {
 }
 
 function addInCartCountChange(value) {
-
-    // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
-
-    // Конец решения задания №1.2.
-
+    let newValue;
+    newValue = oldValue;
+    newValue +1;
     return newValue;
 }
 
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
-    // Задание №1.3. Увеличение суммы в корзине
-
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
-
-    // Конец решения задания №1.3.
-
+    Let newSam;
+    newSam = oldSum + difference;
+    let newSumText = newSum + ' ₽';
+    newSumText = `${newSum} ₽`;
     return newSumText;
 }
 
