be7a4ddd74bc6d47cfe8115bec960a3a33fc77af
Anna DM <floobi@mail.ru>
2021-07-29 06:40:11 +0300

    done2

--- task_2/solution.js
+++ task_2/solution.js
@@ -1,9 +1,9 @@
 function calcShipping(sum, min, shipping) {
-    let productsSum = sum; // сумма в корзине
+    let shippingSum;// сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
 
-    // Задание №2.1. Рассчитать доставку
+    // Задание №2.1 Рассчитать доставку
 
     // создайте переменную shippingSum
     let shippingSum;
@@ -22,13 +22,12 @@ function calcShipping(sum, min, shipping) {
 
     return shippingSum;
 }
-
 function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
 
-    // Задание №2.2. Рассчитать скидку
+    // Задание №2.2 Рассчитать скидку
 
     // создайте переменную discountPrice
     let discountPrice;
@@ -39,11 +38,10 @@ function calcDiscount(sum, min, discount) {
     else {discountSum=0};
 
 
-    // Конец решения задания №2.2.
+    // Конец решения задания №2.2
 
     return discountSum;
 }
-
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
@@ -72,3 +70,4 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
 }
+
