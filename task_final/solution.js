1971080a4bbeee503c919be85a74bbd429ea5b44
Anna DM <floobi@mail.ru>
2021-08-04 13:20:17 +0300

    done_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -10,44 +10,29 @@
 // Объект data содержит все данные
 // В объекте data есть свойства:
 // - client - строка, имя клиента + через дефис телефон клиента;
+// - client - строка, имя клиента + телефон клиента;
 // - order - объект, содержащий данные о заказе:
-//     - address - адрес доставки
-//     - sum - стоимость заказа с учетом скидок и доставки
+// - address - адрес доставки
+// - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
+// - sum - стоимость заказа с учетом скидок и доставки
 // - goods: массив объектов с информацией о позициях заказа:
-//     - title - название позиции
-//     - count - количество в заказе
+// - title - название позиции
+// - count - количество в заказе
 // например:
 // {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
+// "data": {
+// "client": "Иван +7(987)65-43-210",
+// "order": {
+// "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
+// "sum": 900
 //       },
-//       "goods": [
+// "goods": [
 //         {
-//           "title": "Пицца",
-//           "count": 2
+// "title": "Пицца",
+// "count": 2
 //         }
 //      ]
 //    }
 // }
-
 function sendRequest(name, phone, address, goods, sum) {
-    let client = name + " " + phone;
-    let addressForSend = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
-    let order = { address: addressForSend, sum: sum };
-    let GFS = [];
-    let count2 = goods.length;
-
-    for (let i = 0; i < count2; i++) {
-        let good = { title: goods[i].title, count: goods[i].count };
-        GFS.push(good);
-    }
-
-    let data = { client:client, order:order, goods:GFS };
-    let dataForSend = { data : data };
-    let jsonData = JSON.stringify(dataForSend);
-
-    return jsonData;
-}
+    let data = {goods: [], order: {}};
